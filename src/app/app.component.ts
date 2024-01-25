66975037abe625f626df096c2094058164dc7e9c
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2024-01-25 15:22:08 -0300

    create first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import {Component} from '@angular/core';
+import { HomesComponent } from "./homes/homes.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [],
-  template: `
-    <h1>Default</h1>
+    selector: 'app-root',
+    standalone: true,
+    template: `
+    <header class="brand-name">
+      <img src="/assets/logo.svg" alt="logo" aria-hidden="true" class="brand-log">
+    </header>
+    <section class="content">
+      <app-homes></app-homes>
+    </section>
   `,
-  styleUrls: ['./app.component.css'],
+    styleUrls: ['./app.component.css'],
+    imports: [HomesComponent]
 })
 export class AppComponent {
   title = 'default';
---
+++ src/app/homes/homes.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-homes',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      homes works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class HomesComponent {
+
+}
