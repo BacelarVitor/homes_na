86f92f555ab79790c1c7c4f57d81d615849723c5
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2024-01-25 15:32:11 -0300

    input styled

--- src/app/homes/homes.component.ts
+++ src/app/homes/homes.component.ts
@@ -6,13 +6,54 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      homes works!
-    </p>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button type="button" class="primary">Search</button>
+    </form>
   `,
-  styles: [
+  styles: [`
+   .results {
+      display: grid;
+      column-gap: 14px;
+      row-gap: 14px;
+      grid-template-columns: repeat(auto-fill, minmax(400px, 400px));
+      margin-top: 50px;
+      justify-content: space-around;
+    }
+    input[type="text"] {
+      border: solid 1px var(--primary-color);
+      padding: 10px;
+      border-radius: 8px;
+      margin-right: 4px;
+      display: inline-block;
+      width: 30%;
+    }
+    button {
+      padding: 10px;
+      border: solid 1px var(--primary-color);
+      background: var(--primary-color);
+      color: white;
+      border-radius: 8px;
+    }
+    @media (min-width: 500px) and (max-width: 768px) {
+      .results {
+          grid-template-columns: repeat(2, 1fr);
+      }
+      input[type="text"] {
+          width: 70%;
+      }   
+    }
+    @media (max-width: 499px) {
+      .results {
+          grid-template-columns: 1fr;
+      }    
+    }
+  ` 
   ]
 })
 export class HomesComponent {
 
 }
+
+// Create the application’s HousingLocation component
+//https://angular.dev/tutorials/first-app/HousingLocation
